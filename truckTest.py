e86c6e7c1516c2101aabb58b635802f1ec5e8708
Cole Perry <cole.perry113@gmail.com>
2025-04-17 22:09:54 -0400

    Printing all possible PIDs

--- truckTest.py
+++ truckTest.py
@@ -12,6 +12,26 @@ while not connection.is_connected() and time.time() - start_time < timeout:
 	
 if connection.is_connected():
 	print("OBD2 adapter connected.")
+
+	def get_supported_pids(conn):
+		        """Queries the vehicle for supported PIDs in the 01-20 range."""
+		        if conn.is_connected():
+		            response = conn.query(obd.commands.PIDS_A)
+		            if not response.is_null():
+		                supported_pids = []
+		                bit_array = response.value
+		                for i in range(20):
+		                    if bit_array[i]:
+		                        pid_hex = hex(i + 1)[2:].upper().zfill(2)
+		                        supported_pids.append(f"PID 0x{pid_hex}")
+		                return supported_pids
+		            else:
+		                return "Error: Could not retrieve supported PIDs."
+		        else:
+		            return "Error: Not connected to OBD-II adapter."
+		
+	supported_pids_01_20 = get_supported_pids(connection)
+	print("Supported PIDs (01-20):", supported_pids_01_20)
 else:
 	print("Failed to connect to OBD2 adapter")
 	exit()
